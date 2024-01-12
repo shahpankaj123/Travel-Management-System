1ea5ca0fb940d37c1cc525d74bf5ef956067f8e3
Aaryan Shah <aaryanshah615@gmail.com>
2024-01-12 20:01:48 +0545

    staff logic

--- account/admin.py
+++ account/admin.py
@@ -1,35 +1,47 @@
 from django.contrib import admin
 from .models import User
+from django import forms
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 class UserAdmin(BaseUserAdmin):
-    # The fields to be used in displaying the User model.
-    # These override the definitions on the base UserAdmin
-    # that reference specific fields on auth.User.
-    list_display = ["id","email","name","ph","is_admin"]
+    list_display = ["id","email","username","ph","is_admin"]
     list_filter = ["is_admin"]
-    fieldsets = [
-        ("User Credentials", {"fields": ["email", "password"]}),
-        ("Personal info", {"fields": ["name","ph"]}),
-        ("Permissions", {"fields": ["is_superuser","is_admin","is_active","is_merchant","is_staff_member","is_customer_user","groups","user_permissions"]}),
-        ("Token_Verify",{"fields":["tc"]})
-    ]
-    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
-    # overrides get_fieldsets to use this attribute when creating a user.
-    add_fieldsets = [
-        (
-            None,
-            {
-                "classes": ["wide"],
-                "fields": ["email", "name","ph" ,"password1", "password2","is_superuser","is_admin","is_active","is_merchant","is_staff_member","is_customer_user","groups","user_permissions"],
-            },
-        ),
-    ]
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None and request.user.is_staff_member and request.user != obj:
+            return False
+        return super().has_change_permission(request, obj)
+    
+    def get_fieldsets(self, request, obj=None):
+        if obj:
+            if request.user.is_admin:
+                return (
+                    ("User Credentials", {"fields": ["email", "password"]}),
+                    ("Personal info", {"fields": ["username", "ph"]}),
+                    ("Permissions", {"fields": ['is_superuser','is_admin','is_active','is_staff_member','is_merchant','groups','user_permissions']}),
+                    ("Token_Verify", {"fields": ["tc"]}),
+                )
+            elif request.user.is_staff_member:
+                return (
+                    ("User Credentials", {"fields": ["email", "password"]}),
+                    ("Personal info", {"fields": ["username", "ph"]}),
+                    ("Token_Verify", {"fields": ["tc"]}),
+              )
+      
+    
+        return super().get_fieldsets(request, obj)
+    
+    def get_form(self, request, obj=None, **kwargs):
+        if request.user.is_admin:
+            self.add_fieldsets[0][1]['fields'] = ['email','password1','password2','username','ph','is_superuser','is_admin','is_active','is_staff_member','is_merchant','groups','user_permissions']
+        if request.user.is_staff_member:
+            self.add_fieldsets[0][1]['fields'] = ['email','password1','password2','username','ph','is_merchant','is_active','groups']
+        return super().get_form(request, obj, **kwargs)
+    
     search_fields = ["email"]
     ordering = ["email","id"]
     filter_horizontal = []
 
 
-# Now register the new UserAdmin..
 admin.site.register(User,UserAdmin)
 
