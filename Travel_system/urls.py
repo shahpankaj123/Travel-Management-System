587772051d631fd4f12108d46de05495d90bbf36
Aaryan Shah <aaryanshah615@gmail.com>
2024-01-17 19:16:38 +0545

    template stepup

--- Travel_system/urls.py
+++ Travel_system/urls.py
@@ -18,12 +18,15 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
-from .views import Index
+from .views import Index,Contact,About,Services
 
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='adminpannel'),
-    path('',Index,name='Home'),
+    path('',Index,name='home'),
+    path('contact/',Contact,name='contact'),
+    path('about/',About,name='about'),
+    path('services/',Services,name='services')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Travel_system/views.py
+++ Travel_system/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render,redirect
 def Index(request):
     return render(request,'index.html')
 
+def About(request):
+    return render(request,'about.html')
+
+def Services(request):
+    return render(request,'services.html')
+
+def Contact(request):
+    return render(request,'contact.html')
+
         
